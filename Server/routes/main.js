82f41245ad4a1831b557228f5ae4af45f52e24b0
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-10-15 11:10:11 +0100

    Add files via upload

---
+++ Server/routes/main.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+
+//Routes
+// route handler for /blog
+router.get('/Blog', (req, res) => {
+  res.render('Blog', {
+    currentRoute: '/Blog',
+  });
+});
+
+// get method for home route
+router.get('',async (req, res) => {
+    try{
+  const locals = {
+    title: 'Pristine Probizz Blog',
+    description: 'Simple Blog site for the Pristine Probizz website.',
+  }
+  
+  let perPage= 10;
+  let page = req.query.page || 1;
+
+  const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('Blog', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * GET /
+ * Post :id
+*/
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+    title: 'Pristine Probizz Blog',
+    description: 'Simple Blog site for the Pristine Probizz website.',
+    }
+
+    res.render('post', { 
+      locals,
+      data,
+      currentRoute: `/post/${slug}`
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+/**
+ * POST /
+ * Post - searchTerm
+*/
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+
+    res.render("search", {
+      data,
+      locals,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+module.exports = router;
